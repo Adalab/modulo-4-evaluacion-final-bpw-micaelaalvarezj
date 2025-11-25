b9a2dc4bbbd1169518366ddfa083a941de46d122
micaelaalvarezj <micaela.alvarezj@gmail.com>
2025-11-25 19:09:39 +0100

    endpoints put y patch ok

--- src/index.js
+++ src/index.js
@@ -78,3 +78,55 @@ app.post ("/recipe", async (req, res) => {
         res.send("Algo ha ido mal");
     }
 });
+
+app.put("/recipe/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (isNaN(Number(id))) {
+            return res.status(400).json({ error: "La URL es incorrecta: el id debe ser un número" });
+        }
+        //Le pasaremos todos los datos aunque no se hayan modificado todos desde el frontal
+        //Si sigue siendo el mismo lo volvemos a poner
+        const { name, ingredients, instructions } = req.body;
+        if (!name || typeof name !== 'string') {
+                return res.status(400).json({ error: 'El nombre es obligatorio y debe ser un texto' });
+            }
+
+            if (typeof ingredients !== 'string' || ingredients.trim().length === 0 || ingredients.length > 500) {
+                return res.status(400).json({
+                    error: 'Los ingredientes son obligatorios, deben ser texto y no superar los 500 caracteres.'
+                });
+            }
+
+            if (!instructions || typeof instructions !== 'string') {
+                return res.status(400).json({ error: 'Las instrucciones son obligatorias y deben ser un texto' });
+            }
+        //IMPORTANTE: Poner el WHERE para evitar modificar todos los registros
+        const query = "UPDATE recipes SET name = ?, ingredients = ?, instructions = ? WHERE id_recipe = ?"
+        
+        const connection = await mysql.getConnection();
+        await connection.query(query, [name, ingredients, instructions, id]);
+        
+        res.send("Receta actualizada")
+    } catch (error) {
+        console.log(error);
+        res.send("Algo ha ido mal");
+    }
+});
+
+app.patch("/recipe/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (isNaN(Number(id))) {
+            return res.status(400).json({ error: "La URL es incorrecta: el id debe ser un número" });
+        }
+        const query = "UPDATE recipes SET deleted_at = NOW() WHERE id_recipe = ?"
+        const connection = await mysql.getConnection();
+        await connection.query(query, [id]);
+        
+        res.send("Receta eliminada")
+    } catch (error) {
+        console.log(error)
+        res.send("Algo ha ido mal");
+    }
+});
